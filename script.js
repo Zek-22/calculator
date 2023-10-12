acb624aac4cb3267739d36e868c6d5d6a11c1d46
Ezequiel Miranda <mirandaezequiel522@gmail.com>
2023-10-11 22:43:15 -0300

    "JS functionality"

--- script.js
+++ script.js
@@ -23,9 +23,36 @@ let swap = document.getElementById('swap');
 let dot = document.getElementById('dot');
 let equals = document.getElementById('equals');
 
+let history = document.getElementById('history');
 let result = document.getElementById('result');
 
-const buttons = [zero, one, two, three, four, five, six, seven, eight, nine, plus, minus, dot, divide, multiply];
+let historyNumbers = [];
+let historyOperator = [];
+
+const buttons = [zero, one, two, three, four, five, six, seven, eight, nine, dot,percentage];
+
+function calculate(number1, number2) {
+    let historyValue = `${history.value}`;
+    let plus = "+";
+    let minus = "-"
+    let multiply = "*"
+    let divide = "/"
+    let sqrt = "%"
+    if (history.value != '' && result.value != null) {
+        if (historyValue.includes(plus)) {
+            result.value = parseFloat(number1) + parseFloat(number2);
+        } else if (historyValue.includes(minus)) {
+            result.value = parseFloat(number1) - parseFloat(number2);
+        } else if (historyValue.includes(multiply)) {
+            result.value = parseFloat(number1) * parseFloat(number2);
+        } else if (historyValue.includes(divide)) {
+            result.value = parseFloat(number1) / parseFloat(number2);
+        } else if (historyValue.includes(sqrt)) {
+            result.value = (parseFloat(number1) * parseFloat(number2) / 100);
+        }
+    }
+    return result.value;
+}
 
 for (i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function (event) {
@@ -33,10 +60,96 @@ for (i = 0; i < buttons.length; i++) {
     });
 }
 
-deleteOne.addEventListener('click',() => {
+deleteOne.addEventListener('click', () => {
     let currentValue = result.value;
-        if (currentValue.length > 0) {
-            let newValue = currentValue.slice(0, -1);
-            result.value = newValue;
-        }
-    });
+    if (currentValue.length > 0) {
+        let newValue = currentValue.slice(0, -1);
+        result.value = newValue;
+    }
+});
+
+ce.addEventListener('click', () => {
+    result.value = '';
+});
+
+c.addEventListener('click', () => {
+    result.value = '';
+    history.value = '';
+});
+
+pow.addEventListener('click', () => {
+    let currentValue = calculate(history.value,result.value);
+    let newValue = Math.pow(currentValue, 2);
+    result.value = newValue;
+    history.value = `(${currentValue})^2`;
+});
+
+sqrt.addEventListener('click', () => {
+    let currentValue = calculate(history.value,result.value);
+    let newValue = Math.sqrt(currentValue);
+    result.value = newValue;
+    history.value = `√ ${currentValue}`;
+});
+
+plus.addEventListener('click', () => {
+    if (history.value != '' && result.value != '') {
+        history.value = `${calculate(history.value, result.value)} + `;
+    } else {
+        history.value = `${result.value} + `;
+    }
+    result.value = '';
+    historyNumbers.push(result.value);
+    historyOperator.push('+');
+});
+
+minus.addEventListener('click', () => {
+    if (history.value != '' && result.value != '') {
+        history.value = `${calculate(history.value, result.value)} - `;
+    } else {
+        history.value = `${result.value} - `;
+    }
+    result.value = '';
+    historyNumbers.push(result.value);
+    historyOperator.push('-');
+});
+
+multiply.addEventListener('click', () => {
+    if (history.value != '' && result.value != '') {
+        history.value = `${calculate(history.value, result.value)} * `;
+    } else {
+        history.value = `${result.value} * `;
+    }
+    result.value = '';
+    historyNumbers.push(result.value);
+    historyOperator.push('*');
+});
+
+divide.addEventListener('click', () => {
+    if (history.value != '' && result.value != '') {
+        history.value = `${calculate(history.value, result.value)} / `;
+    } else {
+        history.value = `${result.value} / `;
+    }
+    result.value = '';
+    historyNumbers.push(result.value);
+    historyOperator.push('/');
+});
+
+percentage.addEventListener("click", ()=> {
+    if (history.value != '' && result.value != '') {
+        history.value = `${calculate(history.value, result.value)} % `;
+    } else {
+        history.value = `${result.value} % `;
+    }
+    result.value = '';
+    historyNumbers.push(result.value);
+    historyOperator.push('%');
+})
+
+equals.addEventListener('click', ()=> {
+    if (history.value != "" && result.value != "") {
+        calculate(history.value,result.value);
+        history.value = result.value;
+        
+    }
+});
